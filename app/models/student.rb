4632c1a80320a89835566d754fa1ea27f174923b
Farah Rizal <41633573+farahriz@users.noreply.github.com>
2018-12-26 16:47:49 +0800

    Added name and age methods on Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,11 @@
 class Student < ActiveRecord::Base
 # implement your Student model here
+	def name
+		p "#{self.first_name} #{self.last_name}"
+	end
 
+	def age
+		current_age = ((Date.today - self.birthday)/365.25).to_i
+		p current_age
+	end
 end
--- db/config.rb
+++ db/config.rb
@@ -4,3 +4,10 @@ require_relative '../app/models/subject.rb'
 require_relative '../app/models/teacher.rb'
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => "#{File.dirname(__FILE__)}/../db/ar-students.sqlite3")
+
+student = Student.create(first_name: "Jake", last_name: "Jacobs", gender: "Male", birthday: "01-02-1990", email: "jake@jakey.co" , phone: "0128241513")
+
+
+student = Student.first
+p student.name
+p student.age
